1744302dac4b6f76e018b4503dd580d1617ea42e
7hourspg <rajiv840704@gmail.com.com>
2022-10-02 16:39:21 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-## React Mini
-React-mini is the mini and totally fresh version of react it is soo clean....
+##  MY TODO APP
+Created a todo app and it was a wonderful experience 
+
+## SCREENSHOT
+<img src="screenshots/Screenshot (62).png" alt="ss"/>
+
+
+
+<hr>
+
+Made with ❤️ by 7hours
---
+++ src/Dummydata.js
@@ -0,0 +1,18 @@
+let Data = [
+    {
+      name: "task 1",
+      desc: "this is my task",
+      id:1
+    },
+    {
+      name: "task 1",
+      desc: "this is my task",
+      id:2
+    },
+    {
+      name: "task 1",
+      desc: "this is my task",
+      id:3
+    },
+  ];
+  export default Data;
